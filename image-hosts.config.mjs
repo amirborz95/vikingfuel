67c19b32ce02301e4e4bc98a3f57b7e98fa65402
Amir <amirborzlaev@gmail.com>
2026-05-31 12:52:08 +0200

    fix: allow res.cloudinary.com in Next.js image hosts

--- image-hosts.config.mjs
+++ image-hosts.config.mjs
@@ -27,4 +27,8 @@ export const imageHosts = [
         protocol: 'https',
         hostname: 'cdn.corenexis.com',
     },
+    {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+    },
 ];
